9a0940064c3201e562804fba7b600f3f94b67b00
STEEVEPP <116173390+STEEVEPP@users.noreply.github.com>
2023-03-22 21:40:27 +0530

    Update dataset rezising.py

--- dataset rezising.py
+++ dataset rezising.py
@@ -3,7 +3,7 @@ import os
 import os.path
 from PIL import Image
 
-f = r' ' # mention the file path inside the single quotes
+f = r' ' # mention the dataset file path inside the single quotes
 for file in os.listdir(f):
     f_img = f+"/"+file
     img = Image.open(f_img)
